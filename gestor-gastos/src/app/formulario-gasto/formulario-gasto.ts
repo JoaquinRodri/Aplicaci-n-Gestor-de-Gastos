293b51154a94d8af888d966981c4fc5fcced39db
joaquin.rodriguez <jrodrod0901@g.educaand.es>
2026-05-08 14:34:28 +0200

    feat: base del proyecto, servicio, router y navbar

---
+++ gestor-gastos/src/app/formulario-gasto/formulario-gasto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-gasto',
+  imports: [],
+  templateUrl: './formulario-gasto.html',
+  styleUrl: './formulario-gasto.css',
+})
+export class FormularioGastoComponent {}
