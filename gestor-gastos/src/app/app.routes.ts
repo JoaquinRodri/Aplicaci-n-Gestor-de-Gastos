906ee97b861905ccf098b2000feacd0ac2de92a3
daniel.izquierdo <izquierdocuevasdaniel@gmail.com>
2026-05-11 14:20:34 +0200

    Revert "feet: mejoras actuales"
    This reverts commit 7aed834c6eadb7a4dfa03a8c37e8a51380e7a180.

--- gestor-gastos/src/app/app.routes.ts
+++ gestor-gastos/src/app/app.routes.ts
@@ -1,22 +1,21 @@
   import { Routes } from '@angular/router';
   import { LoginComponent } from './login/login';
-  import { authGuard } from './auth-guard'; 
   export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: LoginComponent },
 
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'dashboard', canActivate: [authGuard],
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard',
       loadComponent: () => import('./dashboard/dashboard').then(m => m.DashboardComponent) },
-    { path: 'gastos', canActivate: [authGuard],
+    { path: 'gastos',
       loadComponent: () => import('./lista-gastos/lista-gastos').then(m => m.ListaGastosComponent) },
-    { path: 'gastos/nuevo', canActivate: [authGuard],
+    { path: 'gastos/nuevo',
       loadComponent: () => import('./formulario-gasto/formulario-gasto').then(m => m.FormularioGastoComponent) },
-    { path: 'gastos/:id/editar', canActivate: [authGuard],
+    { path: 'gastos/:id/editar',
       loadComponent: () => import('./formulario-gasto/formulario-gasto').then(m => m.FormularioGastoComponent) },
-    { path: 'gastos/:id', canActivate: [authGuard],
+    { path: 'gastos/:id',
       loadComponent: () => import('./detalle-gasto/detalle-gasto').then(m => m.DetalleGastoComponent) },
-    { path: 'categorias', canActivate: [authGuard],
+    { path: 'categorias',
       loadComponent: () => import('./categorias/categorias').then(m => m.CategoriasComponent) },
-    { path: '**', redirectTo: 'login' }
+    { path: '**', redirectTo: 'dashboard' }
   ];
