4bd6f5cd64e5973c654ca1bc9a3bbd1ae01fb719
daniel.izquierdo <izquierdocuevasdaniel@gmail.com>
2026-05-11 15:09:33 +0200

    feet: auth-guard

--- gestor-gastos/src/app/app.routes.ts
+++ gestor-gastos/src/app/app.routes.ts
@@ -1,6 +1,6 @@
   import { Routes } from '@angular/router';
   import { LoginComponent } from './login/login';
-import { authGuard } from './auth-guard';
+  import { authGuard } from './auth-guard';
   export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: LoginComponent },
