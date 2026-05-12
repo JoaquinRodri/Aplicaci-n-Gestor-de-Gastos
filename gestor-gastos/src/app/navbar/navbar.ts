26d921037a82a7b8e3844d955a0152c4b0f4d402
Marcos Núñez <marcosnunezcuriel@gmail.com>
2026-05-12 12:30:34 +0200

    feature: añadir botón cerrar sesión

--- gestor-gastos/src/app/navbar/navbar.ts
+++ gestor-gastos/src/app/navbar/navbar.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Auth } from '../services/auth'; // Asumiendo la ruta del servicio de la Persona 1
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  // Inyectamos el Router para la navegación y el AuthService para el logout
+  private router = inject(Router);
+  authService = inject(Auth); // 7.1: Inyectar AuthService para acceder al usuario activo
+
+  // 7.1: Método logout
+  logout() {
+    this.authService.logout(); // Llama al método del servicio
+    this.router.navigate(['/login']); // Navega a la página de login
+  }
+}
